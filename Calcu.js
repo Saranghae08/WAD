ced6f7268c28a19ac4a3b9ef8a820798e4532010
Sarah Mae Ponce <Sarahbonapos@gmail.com>
2016-09-01 13:21:06 +0800

    Add files via upload

---
+++ Calcu.js
@@ -0,0 +1,19 @@
+
+var.box=document.getElementById("display");
+
+function display.value+'()
+{
+	box.value +=x;
+
+	if (x=="C")
+	{
+		box.value='';
+	}
+}
+
+function answer()
+{
+	 x=box.value;
+	 x=eval(x);
+	 box.value=x
+}
